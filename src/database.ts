2688dae40120b918a3b86f8494d9582bd2689cf9
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2023-08-02 21:42:40 -0300

    added db into app

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,11 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config)
