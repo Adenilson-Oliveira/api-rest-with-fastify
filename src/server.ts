c87c693bdd044bd4f832a8733ada0d85bfb8ebe2
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2023-08-03 21:18:32 -0300

    added variables of environment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 // import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -23,7 +24,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
